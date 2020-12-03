99a9f2fd930dc7786501d91839e1df2ecc22417b
WhyEtzz <75259101+WhyEtzz@users.noreply.github.com>
2020-12-03 13:06:19 +0700

    Update index.js

--- index.js
+++ index.js
@@ -606,7 +606,7 @@ if (text.includes("!animepict"))
   var path = require('path');
   var text1 = teks.slice(6);
   text1 = suara;
-  var suara = text.replace(/#ttsid/g, text1);
+  var suara = text.replace(/!ttsid/g, text1);
   var filepath = 'mp3/bacot.wav';
   
   
@@ -677,7 +677,7 @@ if (text.includes("!nulis"))
             conn.sendMessage(id, buffer, MessageType.image)
             console.log("done")
          })
-   }
+   };
 
 
         
