534c3e36aa445cd5c005b112018d05ac113f774f
WhyEtzz <75259101+WhyEtzz@users.noreply.github.com>
2020-12-10 10:39:36 +0700

    Update index.js

--- index.js
+++ index.js
@@ -592,8 +592,8 @@ const get = require('got')
    {
     const teks = text.split("!image")[1]
     var items = ["${teks}"];
-    var ${teks} = items[Math.floor(Math.random() * items.length)];
-    var url = "https://api.fdci.se/rep.php?gambar=" + ${teks};
+    var teks = items[Math.floor(Math.random() * items.length)];
+    var url = "https://api.fdci.se/rep.php?gambar=" + teks;
     
     axios.get(url)
       .then((result) => {
