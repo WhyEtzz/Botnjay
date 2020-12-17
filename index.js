82cffc134adc186d034e4ee901730747c3a6e3d8
WhyEtzz <75259101+WhyEtzz@users.noreply.github.com>
2020-12-17 18:54:34 +0700

    Update index.js

--- index.js
+++ index.js
@@ -679,7 +679,37 @@ if (text.includes("!animepict"))
 
 if (text.includes('!ssweb')){
   var teks = text.replace(/!ssweb /, '')
-    axios.get('https://api.apiflash.com/v1/urltoimage?access_key=0c630fb86b394348aaa65629409ea387&url='+teks)
+   if (text.includes('!ssweb')){
+  var teks = text.replace(/!ssweb /, '')
+    const request = require('request');
+const fs = require('fs');
+
+request({
+    url: "https://api.apiflash.com/v1/urltoimage",
+    encoding: "binary",
+    qs: {
+        access_key: "0c630fb86b394348aaa65629409ea387",
+        url: "teks"
+    }
+}, (error, response, body) => {
+    if (error) {
+        console.log(error);
+    } else {
+        fs.writeFile("screenshot.jpeg", body, "binary", error => {
+            console.log(error);
+        });
+    }
+})
+    .then((res) => {
+      imageToBase64(res.data.result)
+        .then(
+          (ress) => {
+            conn.sendMessage(id, 'Proses Nih Babe ^_^', MessageType.text)
+            var buf = Buffer.from(ress, 'base64')
+            conn.sendMessage(id, buf, MessageType.image)
+        })
+     });
+
     .then((res) => {
       imageToBase64(res.data.result)
         .then(
